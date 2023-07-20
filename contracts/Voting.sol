c079efd337cc3b83d491b8ece4976f72f0216e46
Aviroop Jana <aviroopjana@gmail.com>
2023-07-21 03:46:51 +0530

    Update Smart Contract 🔄

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -1,4 +1,4 @@
-//SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.18;
 
@@ -8,41 +8,63 @@ contract Voting {
         uint256 voteCount;
     }
 
-    Candidate [] public candidates;
-    mapping (address => bool) public voters;
+    struct Voter {
+        bool hasVoted;
+        uint256 votedCandidateIndex;
+    }
+
+    Candidate[] public candidates;
+    mapping(address => Voter) public voters;
     address public owner;
 
     uint256 public voteStart;
     uint256 public voteEnd;
 
     constructor(string[] memory _candidateNames, uint256 _durationInMinutes) {
-        for(uint256 i = 0; i<_candidateNames.length; i++ ) {
+        for (uint256 i = 0; i < _candidateNames.length; i++) {
             candidates.push(Candidate(_candidateNames[i], 0));
         }
-        msg.sender == owner;
-        // set voting duration in minutes and convert to seconds
+        owner = msg.sender;
+        // Set voting duration in minutes and convert to seconds
         voteStart = block.timestamp;
         voteEnd = block.timestamp + (_durationInMinutes * 1 minutes);
     }
 
     modifier onlyOwner {
-        require(msg.sender == owner,"Only the owner can call this method");
+        require(msg.sender == owner, "Only the owner can call this method");
         _;
     }
 
-    function addCandidate(string memory _name) public onlyOwner{
-        candidates.push(Candidate({
-            name: _name,
-            voteCount: 0
-        }));
+    modifier onlyDuringVoting {
+        require(
+            block.timestamp >= voteStart && block.timestamp < voteEnd,
+            "Voting is not active"
+        );
+        _;
     }
 
-    function vote(uint256 _candidateIndex) public {
-        require(!voters[msg.sender],"You have already voted");
-        require(_candidateIndex < candidates.length,"Invalid candidate Index");
+    modifier onlyBeforeVoting {
+        require(block.timestamp < voteStart, "Voting has already started");
+        _;
+    }
 
+    modifier onlyAfterVoting {
+        require(block.timestamp >= voteEnd, "Voting is still active");
+        _;
+    }
+
+    function addCandidate(string memory _name) public onlyOwner onlyBeforeVoting {
+        candidates.push(Candidate({name: _name, voteCount: 0}));
+    }
+
+    function vote(uint256 _candidateIndex) public onlyDuringVoting {
+        Voter storage sender = voters[msg.sender];
+        require(!sender.hasVoted, "You have already voted");
+        require(_candidateIndex < candidates.length, "Invalid candidate index");
+
+        sender.hasVoted = true;
+        sender.votedCandidateIndex = _candidateIndex;
         candidates[_candidateIndex].voteCount++;
-        voters[msg.sender] == true;
     }
 
     function getVoteOfAllCandidates() public view returns (Candidate[] memory) {
@@ -50,14 +72,22 @@ contract Voting {
     }
 
     function votingStatus() public view returns (bool) {
-        return(block.timestamp >= voteStart && block.timestamp < voteEnd);
+        return (block.timestamp >= voteStart && block.timestamp < voteEnd);
     }
 
     function remainingTime() public view returns (uint256) {
-        require(block.timestamp >= voteStart, "Voting hasn't started yet!");
-        if(block.timestamp > voteEnd) {
+        if (block.timestamp < voteStart) {
+            return voteStart - block.timestamp;
+        } else if (block.timestamp >= voteEnd) {
             return 0;
+        } else {
+            return voteEnd - block.timestamp;
         }
-        return(voteEnd - block.timestamp);
+    }
+
+    function getVoterVote() public view onlyAfterVoting returns (string memory) {
+        Voter storage sender = voters[msg.sender];
+        require(sender.hasVoted, "You have not voted");
+        return candidates[sender.votedCandidateIndex].name;
     }
 }
